9ff95286b06b387cbbea53490f9318163e90b6f8
Your Name <you@example.com>
2020-05-15 19:32:10 +0800

    update

--- LeetCode/回溯法/leetcode-字符串的所有排列情况.py
+++ LeetCode/回溯法/leetcode-字符串的所有排列情况.py
@@ -6,24 +6,22 @@
 #   给出字符串 s = "abc"，得到所有的排列情况如下：['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
 #   解题思路：回溯法
-#   1、首先我们知道，n个字符的字符串共有n!个排列组合;
-#   2、
-#   3、
-
+#   1、
 
 def permutation(S):
-    res,path = [],''
-    def backtrack(S, path, res):
-        if S == '':
+    res,path = [],''        #   res是全局变量，用来保存每一个全路径；path则表示每次的全路径的值
+    def backtrack(S, path, res):        #   从当前的S中
+        if S == '':             #   结束条件，可以将全路径放到res中了
             res.append(path)
             return
         for i in range(len(S)):
             cur = S[i]
             backtrack(S[:i] + S[i + 1:], path + cur, res)
+            #   S[:i] + S[i + 1:] -> 刨除 S[i] 后的所有字符
     backtrack(S, path, res)
     return res
 
 if __name__ == "__main__":
-    s = 'abc'
+    s = 'aac'
     #print(permutation(s))
     permutation(s)
