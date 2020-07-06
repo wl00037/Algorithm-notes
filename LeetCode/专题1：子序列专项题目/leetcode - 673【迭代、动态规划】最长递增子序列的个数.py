64078bce0228d0a202a8b41324a253d5c3d92fc7
Your Name <you@example.com>
2020-07-06 20:06:36 +0800

    add new

--- LeetCode/专题1：子序列专项题目/leetcode - 673【迭代、动态规划】最长递增子序列的个数.py
+++ LeetCode/专题1：子序列专项题目/leetcode - 673【迭代、动态规划】最长递增子序列的个数.py
@@ -35,6 +35,20 @@ class Solution(object):
                         continue
         return max_len,result
 
+    #   方法二：动态规划
+    #   其实思路和求最长上升子序列所用的动态规划思路一样：
+    #   dp[i]表示：当前元素作为子序列的最后一个元素时，该子序列的最大上升子序列的长度
+    #   然后从dp列表中找到最大的数，并且统计出来即可
+    def findNumberOfLIS(self, nums):
+        curr_max_len = float("-inf")
+        curr_max_len_sublist = []
+        dp = [1] * len(nums)
+        for i, num in enumerate(nums):
+            for j in range(i):  # 每次还需要再遍历一次0-i这个区域，根据每一个索引位的大小，判断此时的num是否可以加在后面；
+                if nums[j] < num:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+
 l1 = [1,1,1,2,2,2,3,3,3]
 l2 = [2,2,2,2,2]
 max_len,result = Solution().findNumberOfLIS(l1)
