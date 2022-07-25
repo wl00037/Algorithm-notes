9714645c68460e10e8eeec98468db9866d5246c5
wl00037 <8196216@163.com>
2022-07-25 21:15:34 +0800

    Update 2、冒泡排序.py

--- 2、排序算法(内部排序、外部排序)/1、内部排序面试题/2、冒泡排序.py
+++ 2、排序算法(内部排序、外部排序)/1、内部排序面试题/2、冒泡排序.py
@@ -3,15 +3,14 @@
 #   外层遍历的索引位置上不断和内层遍历的值比较，保证外层遍历所处的索引位上一定是最小的值；
 #   时间复杂度 O(n^2)
 
-def bubble_sort(nums):
-    if len(nums) == 0:
-        return nums
-    for i in range(0,len(nums)):
-        for j in range(i,len(nums)):
-            if nums[j] < nums[i]:
-                nums[i],nums[j] = nums[j],nums[i]
-    return nums
+def bubble_sort(array):
+    for i in range(0,len(array)):
+        for j in range(i+1,len(array)):
+            if array[i] > array[j]:
+                # index-i和后面所有索引位上的值挨个对比，将最小的一个放到index-i上
+                array[i],array[j] = array[j],array[i]
+    return array
 
-nums = [10,-1,3,2,7,12,-4,0,21,6]
-print(bubble_sort(nums))
-
+if __name__ == "__main__":
+    array = [45,2,61,0,-3,77,13,-53,-23,7]
+    print(bubble_sort(array))
