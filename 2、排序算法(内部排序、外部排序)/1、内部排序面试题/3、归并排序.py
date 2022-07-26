baefdef3eb3f4c40830a5b16c800306dd82c8775
wl00037 <8196216@163.com>
2022-07-26 13:36:49 +0800

    Update 3、归并排序.py

--- 2、排序算法(内部排序、外部排序)/1、内部排序面试题/3、归并排序.py
+++ 2、排序算法(内部排序、外部排序)/1、内部排序面试题/3、归并排序.py
@@ -1,26 +1,39 @@
 #   归并排序
 #   思路：
-#   类似于分治法的思路，将一个数组拆分成
+#   1.将数组从中间划分成两个子数组
+#   2.递归，再将两个子数组分别划分成2个更小的子数组，直到数字中剩下一个元素
+#   3.再将子数组两两排序合并，最终返回一个完整且排序完成的数组
 
-def merge_sort(nums):
-    if len(nums) == 1:
-        return nums
-    curr = []
-    left = merge_sort(nums[0:len(nums)//2])
-    right = merge_sort(nums[len(nums)//2:len(nums)])
+def merge_sort(array):
 
-    while(len(left)!=0 and len(right)!=0):
+    # 当array的长度为1，也就是递归的出口
+    if len(array) == 1:
+        return array
+
+    # left和right是将本次array拆分成两个子数组排序后的结果
+    left = merge_sort(array[0:len(array)//2])
+    right = merge_sort(array[len(array)//2:len(array)])
+
+    current = []
+
+    while(len(left) != 0 and len(right) != 0):
+
         if left[0] < right[0]:
-            curr.append(left.pop(0))
+            current.append(left.pop(0))
         else:
-            curr.append(right.pop(0))
+            current.append(right.pop(0))
 
+    # 当left或right其中一个已经为空，则直接将另一个append到current即可，left和right的排序则完成
     if len(left) != 0:
-        curr.extend(left)
+        current.extend(left)
     else:
-        curr.extend(right)
-    return curr
+        current.extend(right)
 
-nums = [10,-1,3,2,7,12,-4,0,21,6]
-print(merge_sort(nums))
+    return current
 
+if __name__ == "__main__":
+
+    array = [10,-1,3,2,7,12,-4,0,21,6]
+    print(merge_sort(array))
+
+  
